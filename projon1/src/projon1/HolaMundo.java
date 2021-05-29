16a5e062f86c29cbe672e6442b504c87c1362517
jonre jonremen <godfireyt@gmail.com>
2021-05-29 11:57:35 +0200

    Segundo hola

--- projon1/src/projon1/HolaMundo.java
+++ projon1/src/projon1/HolaMundo.java
@@ -5,6 +5,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		
+		System.out.println("Hola 2");
 	}
 
 }
